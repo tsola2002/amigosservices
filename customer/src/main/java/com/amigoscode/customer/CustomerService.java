b2e18d98594ba98b82e610371155e5792cb5de17
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-06-24 19:38:59 +0100

    added fruad microservice

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public record CustomerService(CustomerRepository customerRepository) {
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
