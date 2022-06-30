d32f8f6e3eedd7f82f44d56b0990b3abb3ae6ef3
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-06-30 12:37:19 +0100

    added fraud repository class and refactored fraud & customer microservice

---
+++ fraud/src/main/java/com/amigoscode/fraud/FraudCheckService.java
@@ -0,0 +1,27 @@
+package com.amigoscode.fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class FraudCheckService {
+
+    private final FraudCheckHistoryRepository fraudCheckHistoryRepository;
+
+
+
+    public boolean isFraudulentCustomer(Integer customerId) {
+        // using builder to set parameters
+        fraudCheckHistoryRepository.save(
+                FraudCheckHistory.builder()
+                        .customerId(customerId)
+                        .isFraudster(false)
+                        .createdAt(LocalDateTime.now())
+                        .build()
+        );
+        return false;
+    }
+}
