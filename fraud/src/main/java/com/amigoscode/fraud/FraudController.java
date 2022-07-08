5d9ee6c8a41d64750a0c1a9e79e956ec66d9d91c
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-07-08 13:07:33 +0100

    added spring cloud dependencies

--- fraud/src/main/java/com/amigoscode/fraud/FraudController.java
+++ fraud/src/main/java/com/amigoscode/fraud/FraudController.java
@@ -1,11 +1,13 @@
 package com.amigoscode.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
 
     private final FraudCheckService fraudCheckService;
@@ -14,6 +16,7 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerID) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerID);
+        log.info("fraud check request for customer {}", customerID);
 
         return new FraudCheckResponse(isFraudulentCustomer);
     }
